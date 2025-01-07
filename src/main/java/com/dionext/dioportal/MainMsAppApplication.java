74692279a62098a2471e27ef6cdec402709ed458
dionext <dionextsoft@gmail.com>
2025-01-07 19:41:18 +0300

    move

---
+++ src/main/java/com/dionext/dioportal/MainMsAppApplication.java
@@ -0,0 +1,15 @@
+package com.dionext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MainMsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainMsAppApplication.class, args);
+    }
+
+}
+
